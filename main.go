462385591f4d0f502414e9ed891ba71c26828bf8
White-Fox888 <White.Fox1825@yandex.ru>
2024-10-19 21:54:06 +0500

    Хэширование пароля

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/jackc/pgx/v5"
 )
+
+type TestGrants struct {
+	Grants struct {
+		ID           int    `json:"id"`
+		Title        string `json:"title"`
+		SourceURL    string `json:"source_url"`
+		FilterValues struct {
+			CuttingOffCriteria []interface{} `json:"cutting_off_criteria"`
+			ProjectDirection   []interface{} `json:"project_direction"`
+			Amount             int           `json:"amount"`
+			LegalForm          []interface{} `json:"legal_form"`
+			Age                int           `json:"age"`
+		} `json:"filter_values"`
+	} `json:"grants"`
+}
 
 type Identification struct {
 	Login    string `json:"login"`
@@ -20,8 +39,9 @@ var authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFeHAiOiIyMDIzLTEyLTE4VD
 func main() {
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/check", checkHandler)
-	// http.HandleFunc("/grants", grantsHandler)
+	http.HandleFunc("/grants", grantsHandler)
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,15 +87,45 @@ func checkHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-// func grantsHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.Method != "GET" {
-// 		w.WriteHeader(http.StatusMethodNotAllowed)
-// 		return
-// 	}
+func grantsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
-// 	authHeader := r.Header.Get("Authorization")
-// 	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-// 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-// 		return
-// 	}
-// }
+	conn, err := pgx.Connect(context.Background(), "postgres://dbgr:2110@localhost:5432/dbgr")
+	if err != nil {
+		fmt.Printf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM grants")
+	if err != nil {
+		fmt.Printf("Error with query: %v", err)
+	}
+	defer rows.Close()
+
+	var grant []TestGrants
+
+	for rows.Next() {
+		var grantItem TestGrants
+
+		if err := rows.Scan(&grantItem.Grants.ID, &grantItem.Grants.Title, &grantItem.Grants.SourceURL, &grantItem.Grants.FilterValues.ProjectDirection, &grantItem.Grants.FilterValues.Amount, &grantItem.Grants.FilterValues.LegalForm, &grantItem.Grants.FilterValues.Age, &grantItem.Grants.FilterValues.CuttingOffCriteria); err != nil {
+			fmt.Printf("Error scanning row: %v", err)
+		}
+		grant = append(grant, grantItem)
+	}
+
+	json, err := json.Marshal(grant)
+	if err != nil {
+		fmt.Printf("Error with marshal: %v", err)
+	}
+	fmt.Println(string(json))
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+}
